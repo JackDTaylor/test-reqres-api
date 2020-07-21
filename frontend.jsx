70b05fc4aa62ced4cb436c7592d27662873f3e50
Jack D. Taylor <msec.nt@gmail.com>
2020-07-21 14:53:18 +0300

    Implement basic frontend

--- frontend.jsx
+++ frontend.jsx
@@ -1,1 +1,5 @@
-console.log('Frontend script');
+import ReactDOM from "react-dom";
+import React from "react";
+import Application from "./frontend/Application";
+
+ReactDOM.render(<Application />, document.getElementById('application'));
---
+++ frontend/Application.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Application extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="search">Search</div>
+				<div className="results">Results</div>
+			</div>
+		);
+	}
+}
