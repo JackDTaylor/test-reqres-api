ebdad4aa9721ddd3e427aa65931b0b7e2fe5e88e
Jack D. Taylor <msec.nt@gmail.com>
2020-07-21 11:03:15 +0300

    Add dpr() helper

---
+++ common/dpr.jsx
@@ -0,0 +1,44 @@
+import Util from 'util';
+import {valueType} from "./functions";
+
+global._dpr = function _dpr(values, separator = "\n\n", colors = false) {
+	let stack;
+
+	try {
+		// noinspection ExceptionCaughtLocallyJS
+		throw new Error;
+	} catch(e) {
+		stack = e.stack.split('\n')[3];
+		stack = stack.replace(/^.*\((.*)\).*$/, '$1');
+	}
+
+	const header = `
+##############################################################################
+#
+#    Debug print at
+#      ${stack}
+#
+##############################################################################
+	`.trim() + '\n\n';
+
+	return header + values.map((value, index) => {
+		let title = `####### Index ${index} #`.padEnd(78, '#') + '\n';
+
+		if(typeof value === "undefined") {
+			return title + "undefined";
+		}
+
+		if([Number, String].indexOf(valueType(value)) >= 0) {
+			return title + value;
+		}
+
+		return title + Util.inspect(value, {depth: 5, colors});
+	}).join(separator);
+};
+
+export const dpr = function(...values) {
+	const error = new Error;
+	error.stack = _dpr(values);
+
+	throw error;
+};
