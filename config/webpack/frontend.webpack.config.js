f76d07b876d24f147ea8159ec15fc43c87529b99
Jack D. Taylor <msec.nt@gmail.com>
2020-07-22 01:27:52 +0300

    Minify frontend bundle

--- config/webpack/frontend.webpack.config.js
+++ config/webpack/frontend.webpack.config.js
@@ -1,1 +1,19 @@
-module.exports = {};
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+	"plugins[]": [
+		new UglifyJsPlugin({
+			cache: true,
+			parallel: true,
+			sourceMap: true,
+			uglifyOptions: {
+				ecma: 6,
+				compress: true,
+				mangle: {
+					keep_fnames: true,
+				},
+			}
+
+		}),
+	]
+};
