b0aabf7e2e00166735a34a107bce76ca77e89292
Jack D. Taylor <msec.nt@gmail.com>
2020-07-21 14:53:32 +0300

    Add Router.registerController method

--- backend/Router.jsx
+++ backend/Router.jsx
@@ -1,14 +1,27 @@
-export default class Router {
+import Component from "./Component";
+
+export default class Router extends Component {
 	route(Controller, endpointName) {
 		return async(request, response) => {
 			try {
-				const controller = new Controller(request, response);
+				const controller = new Controller(this.application, request, response);
 
 				return await controller.handleEndpoint(endpointName);
 			} catch(error) {
 				response.set({'Content-Type': 'text/plain'});
 				response.send(error.stack);
 			}
+		}
+	}
+
+	registerController(Controller) {
+		for(const route of Controller.Routes || []) {
+			const method = String(route.httpMethod || 'get').toLowerCase();
+
+			const url = `${Controller.BaseUrl}${route.url}`;
+			const routeFn = this.route(Controller, route.endpoint);
+
+			this.application.express[method].apply(this.application.express, [url, routeFn]);
 		}
 	}
 }
